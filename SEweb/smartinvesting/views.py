04b1f4bc01b0fad6a5f275bde161f9c3fc1f3c53
tewlyfool <32865933+tewlyfool@users.noreply.github.com>
2019-09-30 00:20:26 +0700

    Update views.py
    เปลี่ยน model

--- SEweb/smartinvesting/views.py
+++ SEweb/smartinvesting/views.py
@@ -6,7 +6,27 @@ import pandas_datareader as pdr
 import datetime
 import pandas as pd
 import numpy as np
+from sklearn.base import BaseEstimator, TransformerMixin
 
+class GaussianFeatures(BaseEstimator, TransformerMixin):
+
+    def __init__(self, N, width_factor=2.0):
+        self.N = N
+        self.width_factor = width_factor
+
+    @staticmethod
+    def _gauss_basis(x, y, width, axis=None):
+        arg = (x - y) / width
+        return np.exp(-0.5 * np.sum(arg ** 2, axis))
+
+    def fit(self, X, y=None):
+        # create N centers spread along the data range
+        self.centers_ = np.linspace(X.min(), X.max(), self.N)
+        self.width_ = self.width_factor * (self.centers_[1] - self.centers_[0])
+        return self
+
+    def transform(self, X):
+        return self._gauss_basis(X[:, :, np.newaxis], self.centers_,self.width_, axis=1)
 # Create your views here.
 def index(req):
 
@@ -21,7 +41,7 @@ def cal(req):
         s =  datetime.datetime.strptime(ss, '%Y-%m-%d') 
         e =  datetime.datetime.strptime(se, '%Y-%m-%d' )  
         h = data['set50'] + '.BK'
-        poly_model = make_pipeline(PolynomialFeatures(5), LinearRegression())
+        poly_model = make_pipeline(GaussianFeatures(18), LinearRegression())
         pttgc = pdr.get_data_yahoo(h.upper(), start = datetime.datetime(1999, 9, 5 ), end = datetime.date.today())
         kk = pttgc[['Adj Close']]
         kk = kk.values
@@ -35,7 +55,7 @@ def cal(req):
         s = s.toordinal()
         e = e.toordinal()
         # xf = [[i] for i in range(s,e+1)]
-        table = [[datetime.date.fromordinal(i).strftime('%A %d-%m-%Y'),'-' if len(pttgc[pttgc.index.date==datetime.date.fromordinal(i)]['Adj Close'].values)==0 else round(pttgc[pttgc.index.date==datetime.date.fromordinal(i)]['Adj Close'].values[0],3) ,round(poly_model.predict([[i]])[0],3)]  for i in range(s,e+1)]
+        table = [[datetime.date.fromordinal(i).strftime('%A %d-%m-%Y'),'-' if len(pttgc[pttgc.index.date==datetime.date.fromordinal(i)]['Adj Close'].values)==0 else round(pttgc[pttgc.index.date==datetime.date.fromordinal(i)]['Adj Close'].values[0],3) ,round(poly_model.predict(np.asarray([[i]]))[0],3)]  for i in range(s,e+1)]
         print(table)
         out['result'] = table
         out['set'] = data['set50'].upper()
